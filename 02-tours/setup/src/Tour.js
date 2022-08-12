0867156e6e78211744e84273c29ef09fb88f4946
kareemadesola <kareemadesola1999@gmail.com>
2022-08-12 18:38:31 +0100

    create structure for tour

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ id, name, info, image, price }) => {
+  return (
+    <article key={id} className="single-tour">
+      <img src={image} alt={name} />
+      <div className="tour-info">
+        <h4>{name}</h4>
+        <h4 className="tour-price">${price}</h4>
+      </div>
+      <p>{info}</p>
+      <button className="delete-btn">not interested</button>
+    </article>
+  );
 };
 
 export default Tour;

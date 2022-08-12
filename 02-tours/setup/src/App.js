8496e794d7fb4719b781116db774876dc37c33d0
kareemadesola <kareemadesola1999@gmail.com>
2022-08-12 18:48:34 +0100

    create structure for app

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,43 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
+
+const Error = () => (
+  <div className="error">
+    <h1>Error...</h1>
+  </div>
+);
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [tours, setTours] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json();
+        } else {
+          setIsLoading(false);
+          setIsError(true);
+          throw new Error(resp.statusText);
+        }
+      })
+      .then((tours) => {
+        setTours(tours.parse());
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+  return (
+    <>
+      {isLoading ? <Loading /> : isError ? <Error /> : <Tours tours={tours} />}
+    </>
+  );
 }
 
-export default App
+export default App;
